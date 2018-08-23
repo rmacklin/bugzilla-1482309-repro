e2af06d7918d46e0abf82f4660106c3936ef09a4
Richard Macklin <richard.github@nrm.com>
2018-08-22 19:02:01 -0700

    Add CORS response headers in login_sessions#create

--- auth/app/controllers/login_sessions_controller.rb
+++ auth/app/controllers/login_sessions_controller.rb
@@ -2,6 +2,9 @@ class LoginSessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
+    headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+    headers['Access-Control-Allow-Credentials'] = 'true'
+
     app_callback_url = 'http://localhost:3000/user_sessions/o_auth_callback'
 
     redirect_to app_callback_url
