ef4375584a741628bdd8696e6b0ead55ee4d079a
Richard Macklin <richard.github@nrm.com>
2018-08-22 19:13:31 -0700

    Add CORS response headers in user_sessions#o_auth_callback

--- app/app/controllers/user_sessions_controller.rb
+++ app/app/controllers/user_sessions_controller.rb
@@ -1,5 +1,8 @@
 class UserSessionsController < ApplicationController
   def o_auth_callback
+    headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+    headers['Access-Control-Allow-Credentials'] = 'true'
+
     render json: { token: 'FOO' }
   end
 end
